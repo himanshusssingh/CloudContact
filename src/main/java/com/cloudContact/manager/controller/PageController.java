c42f39c18370ed9c23972c64c5e97aaf19e3693e
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-04 17:56:42 +0530

    add home page controller

--- src/main/java/com/cloudContact/manager/controller/PageController.java
+++ src/main/java/com/cloudContact/manager/controller/PageController.java
@@ -1,5 +1,14 @@
 package com.cloudContact.manager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PageController {
 
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("----------Home page handler-----------");
+        return "home";
+    }
 }
