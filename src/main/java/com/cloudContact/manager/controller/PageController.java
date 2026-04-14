10731c8c561ff2fc327d208b1edd6cd49477c90e
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-14 11:58:14 +0530

    check controllers

--- src/main/java/com/cloudContact/manager/controller/PageController.java
+++ src/main/java/com/cloudContact/manager/controller/PageController.java
@@ -1,14 +1,17 @@
 package com.cloudContact.manager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("----------Home page handler-----------");
+        model.addAttribute("name", "Himanshu Singh" );
+        model.addAttribute("age", "20");
         return "home";
     }
 }
