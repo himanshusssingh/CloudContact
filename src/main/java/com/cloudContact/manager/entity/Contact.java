bf84ae8d4156de41afbb0a7a4e353fad7d53f98c
himanshusssingh <himanshusingh39397@gmail.com>
2026-04-17 23:23:18 +0530

    remove unused package

--- src/main/java/com/cloudContact/manager/entity/Contact.java
+++ src/main/java/com/cloudContact/manager/entity/Contact.java
@@ -3,12 +3,10 @@ package com.cloudContact.manager.entity;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
